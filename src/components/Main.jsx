0895f03f5f72c2273b260e98a9e719c89059bacd
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-08-25 15:40:17 +0200

    [feat] add Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,37 @@
+const Main = () => {
+  return (
+    <main>
+      <div className="content">
+        <div className="container">
+          <h2> --&gt; Content goes here &lt;-- </h2>
+        </div>
+      </div>
+      <div className="body">
+        <div className="container d-flex">
+          <div className="col product">
+            <img src="../public/img/buy-comics-digital-comics.png" alt="" />
+            <span>DIGITAL COMICS</span>
+          </div>
+          <div className="col product">
+            <img src="../public/img/buy-comics-merchandise.png" alt="" />
+            <span>DC MERCHANDISE</span>
+          </div>
+          <div className="col product">
+            <img src="../public/img/buy-comics-subscriptions.png" alt="" />
+            <span>SUBSCRIPTIONS</span>
+          </div>
+          <div className="col product">
+            <img src="../public/img/buy-comics-shop-locator.png" alt="" />
+            <span>COMIC SHOP LOCATION</span>
+          </div>
+          <div className="col product">
+            <img src="../public/img/buy-dc-power-visa.svg" alt="" />
+            <span>DC POWER VISA</span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
