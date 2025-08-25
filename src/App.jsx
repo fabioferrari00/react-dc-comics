04d0d8ea8ebe320f0c621240e0ccef2fc07558c4
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-08-25 15:01:21 +0200

    [feat] add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+//importo le componenti
+import Header from "./components/Header";
+
 
 function App() {
-
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+  return (
+    <header className="container">
+      <div id="logo">
+        <img src="../public/img/dc-logo.png" alt="" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Collectibilies</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">Fans</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Shop</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
